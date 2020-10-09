e9c7ba9e8af3b1dd26874b6499fdaa944a59f944
Eder Canada <canada.eder@gmail.com>
2020-10-09 11:06:08 -0700

    get api

--- app.js
+++ app.js
@@ -1,13 +1,28 @@
+const fs = require('fs')
 const express = require('express')
 
 const app = express()
 
-app.get('/', (req,res)=>{
-  res.status(200).json({message: "hello world", app:"nature tours"})
-})
+// app.get('/', (req,res)=>{
+//   res.status(200).json({message: "hello world", app:"nature tours"})
+// })
 
-app.post('/', (req,res)=>{
-  res.send('you can post to this input')
+// app.post('/', (req,res)=>{
+//   res.send('you can post to this input')
+// })
+
+ 
+const tours = JSON.parse(fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`))
+
+
+app.get('/api/v1/tours', (req,res)=>{
+ res.status(200).json({
+   status: 'success',
+   results: tours.length, 
+   data: {
+    tours: tours // this is the tours simple 
+   }
+ })
 })
 
 const port = 3001;
@@ -15,5 +30,3 @@ app.listen(port, ()=>{
   console.log(`app running on port: ${port}`)
 })
 
-
-[um_loggedin] Welcome back {first_name} {last_name}: {username}, here is what has been happening recently [/um_loggedin]
