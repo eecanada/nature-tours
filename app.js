7904133b07c1d0b83d1f701186e43e40d6ca8740
Eder Canada <canada.eder@gmail.com>
2020-10-13 11:12:38 -0700

    psot request

--- app.js
+++ app.js
@@ -3,6 +3,9 @@ const express = require('express')
 
 const app = express()
 
+//middleware, modify incoming request data
+app.use(express.json())
+
 // app.get('/', (req,res)=>{
 //   res.status(200).json({message: "hello world", app:"nature tours"})
 // })
@@ -11,9 +14,8 @@ const app = express()
 //   res.send('you can post to this input')
 // })
 
- 
-const tours = JSON.parse(fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`))
 
+const tours = JSON.parse(fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`))
 
 app.get('/api/v1/tours', (req,res)=>{
  res.status(200).json({
@@ -23,6 +25,25 @@ app.get('/api/v1/tours', (req,res)=>{
     tours: tours // this is the tours simple 
    }
  })
+})
+
+
+app.post('/api/v1/tours', (req,res)=>{
+  console.log(req.body, 'hit')
+
+  const newId = tours[tours.length - 1].id++
+  const newTour = Object.assign({id: newId}, req.body)
+
+  tours.push(newTour)
+  //stringify turns javascript object to json object
+  fs.writeFile(`${__dirname}/dev-data/data/tours-simple.json`, JSON.stringify(tours), err =>{
+    res.status(201).json({
+      status: 'success',
+      data: {
+        tours: newTour
+      }
+    })
+  })
 })
 
 const port = 3001;
