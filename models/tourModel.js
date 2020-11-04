b17d6698198613efeac31520df74070692a3a9d8
Eder Canada <canada.eder@gmail.com>
2020-11-04 11:04:11 -0800

    update schema

--- models/tourModel.js
+++ models/tourModel.js
@@ -5,16 +5,56 @@ const tourSchema = new mongoose.Schema({
   name: {
     type: String,
     require: [true, 'A tour must have a name'],
-    unique: true 
+    unique: true,
+    trim: true
+ 
   },
-  rating: {
-    Number,
+  duration: {
+    type: Number,
+    require: [true, 'A tour must have a duration' ]
+  },
+  MaxGroupSize: {
+    type: Number,
+    require: [true, 'A tour must have a group size']
+  },
+  difficulty: {
+    type: String,
+    require: [true, 'A tour must have a difficulty']
+  },
+  ratingsAverage: {
+    type: Number,
+    default: 4.5
     // require: [true, 'A tour must have a rating']
   },
+  ratingQuantity: {
+    type: Number,
+    default: 0
+  }, 
   price:{
     type: Number,
     require: [true, 'A tour must have a price'] 
-  } 
+  },
+  priceDiscount: Number, 
+  summary: {
+    type: String,
+    trim: true,
+    required: [true, 'A tour must have a summary']
+  },
+  description: {
+    type: String,
+    trim: true
+  },
+  imageCover:{
+    type: String,
+    required: [true, 'A tour must have a cover image']
+  },
+  images: [String],
+  createdAt: {
+    type: Date,
+    default: Date.now()
+  },
+  startDates: [Date]
+  
 })
 
 //MODEL
